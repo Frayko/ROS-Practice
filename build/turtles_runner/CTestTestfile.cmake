1e8da2114312668088967fc70e2c688de09a5fa4
Alex <57676076+Frayko@users.noreply.github.com>
2023-05-09 20:37:21 +0700

    Delete build folder

--- build/turtles_runner/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alex/roslab/src/turtles_runner
-# Build directory: /home/alex/roslab/build/turtles_runner
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
